65db93c6b6d51a1bb468229f95fda9ac2d995ccd
Asyps <105502574+Asyps@users.noreply.github.com>
2024-10-31 13:19:08 +0100

    idk

--- src/main/java/com/example/pepehohvezdnytym1/HelloApplication.java
+++ src/main/java/com/example/pepehohvezdnytym1/HelloApplication.java
@@ -1,11 +1,16 @@
 package com.example.pepehohvezdnytym1;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -15,7 +20,22 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240, Color.BLUE);
         stage.setScene(scene);
         stage.show();
+
+        public void displayImage() {
+            FileChooser fc = new FileChooser();
+            File file = fc.showOpenDialog(stage);
+
+
+            myImageView.setImage();
+        }
     }
+
+
+    @FXML
+    ImageView myImageView;
+    Button myButton;
+
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/example/pepehohvezdnytym1/HelloController.java
+++ src/main/java/com/example/pepehohvezdnytym1/HelloController.java
@@ -2,13 +2,12 @@ package com.example.pepehohvezdnytym1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.io.File;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
